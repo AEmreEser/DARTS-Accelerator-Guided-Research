5d5eb59c96809797595ecce34de2348859790beb
A. Emre Eser <101353048+AEmreEser@users.noreply.github.com>
2025-06-09 17:17:44 +0300

    Delete fpmac/vlmod directory

--- fpmac/vlmod/tb_fpmac.d
+++
@@ -1,9 +0,0 @@
-tb_fpmac.o: ../tb/tb_fpmac.cpp \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilatedos.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_config.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_types.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_funcs.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_vcd_c.h \
-  /opt/homebrew/Cellar/verilator/5.032/share/verilator/include/verilated_trace.h \
-  Vfpmac___024root.h Vfpmac.h
